7b4fe4954a2f9e4bde49320caf9baf58f77337db
maksimilian378 <44581247+maksimilian378@users.noreply.github.com>
2019-08-18 17:37:40 +0300

    Add files via upload

---
+++ uncrate/script.js
@@ -0,0 +1,13 @@
+// открытие, закрытие блоков
+
+$(document).ready(function(){
+
+   $('.left-item').click(function(){
+      $(this).toggleClass('opana')
+   });
+
+   $('.right-item').click(function(){
+      $(this).toggleClass('opana2')
+   }); 
+});
+
